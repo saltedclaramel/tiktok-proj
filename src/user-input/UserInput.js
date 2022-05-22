45b7b0e897be618f77201fb92fec5dda5ea7f0a3
clara <saltedclaramel19@gmail.com>
2022-05-22 16:26:57 +0800

    created keyboard

--- src/user-input/UserInput.js
+++ src/user-input/UserInput.js
@@ -6,9 +6,41 @@ class UserInput extends Component {
     //     super(props);
     // }
     render() {
+        const keyboardKeysRow1 = [
+            'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'
+        ]
+        const keyboardKeysRow2 = [
+            'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'
+        ]
+        const keyboardKeysRow3 = [
+            'Z', 'X', 'C', 'V', 'B', 'N', 'M'
+        ]
       return (
-          <div>
-              <p>user input</p>
+          <div className='keyboard'>
+                <div>
+                    { keyboardKeysRow1.map((keyboardKey) =>
+                        <button>
+                            { keyboardKey }
+                        </button>
+                    )}
+                </div>
+                <div>
+                    { keyboardKeysRow2.map((keyboardKey) =>
+                        <button>
+                            { keyboardKey }
+                        </button>
+                    )}
+                </div>
+                <div>
+                    { keyboardKeysRow3.map((keyboardKey) =>
+                        <button>
+                            { keyboardKey }
+                        </button>
+                    )}
+                </div>
+                
+                  
+
           </div>
       );
     }
